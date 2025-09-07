8c661734892f0b70e6b4f6e3179afa51b32b9578
ricksva <126216160+ricksva@users.noreply.github.com>
2025-09-07 19:08:01 +0000

    Deploying to gh-pages from @ ricksva/ricksva.github.io@e12cfe85cb166a973cd529261345da483e523fa3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -89,13 +89,13 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
+          section: "News",},{id: "projects-vsd-vad-modules",
+          title: 'VSD/VAD modules',
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
+            },},{id: "projects-computer-ethics-pedagogy",
+          title: 'computer ethics pedagogy',
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
