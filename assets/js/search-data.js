8bedf111a81e906bc182478dc16cb4646b54d03f
ricksva <126216160+ricksva@users.noreply.github.com>
2025-09-07 19:09:38 +0000

    Deploying to gh-pages from @ ricksva/ricksva.github.io@405f2c67a0c67a9bf511f283aed68a6c63ef6981 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -94,13 +94,13 @@ ninja.data = [{
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
+            },},{id: "projects-computer-ethics-pedagogy",
+          title: 'computer ethics pedagogy',
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
+            },},{id: "projects-parasociality-conversational-agents-and-robots",
+          title: 'parasociality, conversational agents, and robots',
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
