edb08968de83db2bef04a5cd02aa6aa9515f97f6
ricksva <126216160+ricksva@users.noreply.github.com>
2025-09-08 20:34:44 +0000

    Deploying to gh-pages from @ ricksva/ricksva.io@05a0b215cc661878828952665148a73cf6f1adc9 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "ricksva/ricksva.github.io",
+    "data-repo": "ricksva/ricksva.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
